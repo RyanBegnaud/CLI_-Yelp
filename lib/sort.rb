93c16fa2d02999fbc486eaa2e653bf63900f97e3
RyanBegnaud <68866254+RyanBegnaud@users.noreply.github.com>
2020-09-02 12:33:08 -0700

    Update sort.rb

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,43 +1,28 @@
-
 require_relative "../lib/business.rb"
+
 class Sort
     @@all = []
     
     def self.businesses(businesses)
         businesses.each do |business|
-         if business["is_closed"] == false 
-            business["name"] = Business.new(business["name"])
-            business["name"].address = business["location"]["display_address"]
-            business["name"].phone_number = business["display_phone"]
-            business["name"].rating = business["rating"]
-            business["name"].food_type = business["categories"]
-            business["name"].price = business["price"]
-            business["name"].website = business["url"]
-            business["name"].review_count = business["review_count"]
+            if business["is_closed"] == false 
+                business["name"] = Business.new(business["name"])
+                business["name"].address = business["location"]["display_address"]
+                business["name"].phone_number = business["display_phone"]
+                business["name"].rating = business["rating"]
+                business["name"].food_type = business["categories"]
+                business["name"].price = business["price"]
+                business["name"].website = business["url"]
+                business["name"].review_count = business["review_count"]
              
-            # list_hash = {
-            #     Name: business["name"],
-            #     Address: business["location"]["display_address"],
-            #     Phone_Number: business["display_phone"],
-            #     Rating: business["rating"],
-            #     FoodType: business["categories"],
-            #     Price: business["price"],
-            #     Website: business["url"]
-            # }
-            @@all << business["name"]
-         end
+                @@all << business["name"]
+            end
         end
+        self.sort
     end
     
     def self.sort 
-        i = 0
-        o = 1
-        @@all.each_with_index.map do |business, i|
-            @@all[i].rating <=> @@all[o].rating
-            if o < @@all.length - 1
-                 o += 1
-            end
-        end
+       @@all = @@all.sort_by(&:rating).reverse 
     end
 
     def self.all 
