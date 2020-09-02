c7712854f02fd4a5cd1c00f7e0c5d3d3d2b9c7fd
RyanBegnaud <68866254+RyanBegnaud@users.noreply.github.com>
2020-09-02 12:32:16 -0700

    Update display.rb

--- lib/display.rb
+++ lib/display.rb
@@ -1,13 +1,8 @@
-
 class Display
     def self.display 
-        i = 1
-        
-        Sort.all.each do |business|
-            puts "\n#{i}. Name: #{business.name} \n   Category: #{business.food_type[0]["title"]} \n   Rating: *#{business.rating} \n   Review Count: ##{business.review_count} \n   Phone Number:#{business.phone_number} 
+        Sort.all.each.with_index(1) do |business, index|
+            puts "\n#{index}. Name: #{business.name} \n   Category: #{business.food_type[0]["title"]} \n   Rating: *#{business.rating} \n   Review Count: ##{business.review_count} \n   Phone Number:#{business.phone_number} 
    Price: #{business.price} \n   Address: #{business.address.join(" ")} \n   Website: #{business.website}" 
-            
-            i+=1
-         end
-     end
+        end
+    end
 end
